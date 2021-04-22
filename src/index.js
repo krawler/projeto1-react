9d267edd2889819476cc0e0039db3ca4a9ff2408
raframos@deloitte.com-at-766691614904 <raframos@deloitte.com-at-766691614904>
2021-04-22 17:04:37 -0300

    novas alterações e testes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './templates/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/artigo" component={Home} />
+        <Home />
+      </Switch>      
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/templates/Home/Home.test.js
+++ src/templates/Home/Home.test.js
@@ -1,7 +1,51 @@
-describe('<Home />', ()=>{
+import { render, waitForElementToBeRemoved } from "@testing-library/react"
+import Home from "../artigo"
+import { rest } from "msw"
+import { setupServer } from "msw/node"
 
-    it('Teste 1',() => {
-       expect(1).toBe(1);
+const handlers = [
+    rest.get('https://jsonplaceholder.typicode.com/posts', async (req,res,ctx) => {
+        return res(ctx.json([
+            {
+                userId: 1,
+                id: 1,
+                title: 'title1',
+                body: 'Body context blog1',
+            },
+            {
+                userId: 2,
+                id: 2,
+                title: 'title2',
+                body: 'Body context blog2',
+            },
+            {
+                userId: 3,
+                id: 3,
+                title: 'title3',
+                body: 'Body context blog3',
+            },
+        ]));
+    })
+];
+
+const server = setupServer(...handlers);
+
+describe('<Home />', ()=>{
+   
+    beforeAll(() => {
+        server.listen()
+    });
+
+    it('should render search, posts and load more', async () => {
+        render(<Home />)
+        const noMorePosts = screen.getByText('Não existem posts!')
+
+        await waitForElementToBeRemoved(noMorePosts);
+        screen.debug();
+    });
+
+    afterAll(() => {
+        server.close
     });
  
-})
+});
